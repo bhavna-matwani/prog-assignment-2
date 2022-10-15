3f703d17909aacf3fe33cc31053c050e93f1b449
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-10-14 22:35:14 -0400

    Update problem1.m

--- problem1.m
+++ problem1.m
@@ -1,13 +1,13 @@
 Q=Q.'
-[Found,P]=InvertMidpoints(Q)
+[Found,P]=InvertMidpoints(Q);
 Found
 P.'
+
 function [Found,P]=InvertMidpoints(Q)
     Found=true;
     [rowx_size,colx_size]=size(Q);
-    size(Q)
-    N=rowx_size
-    
+    N=rowx_size;
+    % generating c matrix
     C=zeros(N:N);
     cnt=1;
     while(cnt<=N)
@@ -22,37 +22,30 @@ function [Found,P]=InvertMidpoints(Q)
     end
     
     if(rem(rowx_size,2))
+        % generating px
         P(:,1)=C\Q(:,1);
+        % generating py
         P(:,2)=C\Q(:,2);
     else
-        flag=0;
-        i=0;
-        while(i<N)
-            d=C;
-            d(N-i,:)=[];
-            t=Q;
-            t(N-i,:)=[];
-            P(:,1)=d\t(:,1);
-            P(:,2)=d\t(:,2);
-            Q1(:,1)=C*P(:,1);
-            Q1(:,2)=C*P(:,2);
-            j=1;
-            while(j<=N)
-                ex=abs(Q(j,1)-Q1(j,1))/Q(j,1);
-                ey=abs(Q(j,2)-Q1(j,2))/Q(j,2);
-                if(ex>10^(-8)| ey>10^(-8))
-                    Found=false;
-                    P=zeros(N,2);
-                    flag=1;
-                    break;
-                end
-                j=j+1;
-            end
-            if(flag==0)
-                break;
-            end
-            i=i+1;
-        end   
-        
+        % making a copy of C matrix
+        d=C;
+        d(1,:)=[];
+        % making a copy of Q matrix
+        t=Q;
+        t(1,:)=[];
+        % generating Px
+        P(:,1)=d\t(:,1);
+        % generating Py
+        P(:,2)=d\t(:,2);
+        % generating Q matrix using new values of P
+        x=(P(1,1)+P(2,1))/2;
+        y=(P(1,2)+P(2,2))/2;
+        %calculating error for experimental Q and theoretical Q
+        ex=abs(x-Q(1,1));
+        ey=abs(y-Q(1,2));
+        if(ex>10^-8 | ey>10^-8)
+            Found=false;
+            P=zeros(N,2);
+        end
     end
-endv
+end
